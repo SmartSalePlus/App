fc08165f692e2762ed154e233ee5ccadb611037d
Ramil98 <y-ramil2013@yandex.ru>
2025-01-25 21:59:16 +0700

    Create

---
+++ SmartSaleApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartSaleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SmartSaleApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartSaleApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
